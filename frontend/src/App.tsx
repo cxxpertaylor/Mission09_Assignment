f68c53a6a11c1cb0729a386816e560e82e538a75
cxxpertaylor <cooper.b.taylor@gmail.com>
2025-03-03 16:15:49 -0700

    Added comments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css';
+// Import data from external json file
 import data from './CollegeBasketballTeams.json';
 
+// Save data from external json file (data) to new variable teamData
 const teamData = data['teams'];
 
+// Component to display the welcome message
 function Welcome() {
   return <h1>NCAA Basketball Teams</h1>;
 }
 
+// Function to take the info for a team and print it out in a nice "card".
+// Parameters include school, name (mascot), city, and state
 function TeamCard({
   school,
   name,
@@ -30,10 +35,12 @@ function TeamCard({
   );
 }
 
+// Function to take the teamData and send each entry to a TeamCard using the .map() function
 function TeamList() {
   return teamData.map((team) => <TeamCard {...team} />);
 }
 
+// Display the Welcome component and the TeamList
 function App() {
   return (
     <>
